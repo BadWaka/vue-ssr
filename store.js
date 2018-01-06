7f1d235af62c492c99d88371c98b3362b9321015
BadWaka <1456683844@qq.com>
2018-01-06 15:41:38 +0800

    bak

---
+++ store.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+// 假定我们有一个可以返回 Promise 的通用 API（请忽略此 API 具体实现细节）
+import {fetchItem} from './api';
+export function createStore() {
+    return new Vuex.Store({
+        state: {
+            items: {}
+        },
+        actions: {
+            fetchItem({
+                commit
+            }, id) {}
+        },
+        mutations: {}
+    });
+};
