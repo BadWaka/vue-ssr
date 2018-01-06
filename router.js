401683e22fbb2d5a40ab176601289f1a1d809df6
BadWaka <1456683844@qq.com>
2018-01-06 15:35:28 +0800

    路由和代码分割

--- router.js
+++ router.js
@@ -3,5 +3,20 @@ import Router from 'vue-router';
 Vue.use(Router);
 
 export function createRouter() {
-    return new Router({mode: 'history', routes: []});
+    return new Router({
+        mode: 'history',
+        routes: [
+            {
+                path: '/',
+                component: () => {
+                    import ('./components/Home.vue')
+                }
+            }, {
+                path: '/item/:id',
+                component: () => {
+                    import ('./components/Item.vue')
+                }
+            }
+        ]
+    });
 };
